ded10fd704ffefc9f04688d82aa26598814ea942
DeveloperOk <ok.fahrettin@gmail.com>
2024-12-25 15:58:18 +0300

    Initial commit.

---
+++ app/src/main/java/com/enterprise/gif/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.enterprise.gif
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.enterprise.gif.ui.theme.GIFTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GIFTheme {
+                GIFApp()
+            }
+        }
+    }
+}
+
+
+
+@Composable
+fun GIFApp() {
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top,
+        modifier = Modifier.fillMaxSize().background(color = Color.Green)){
+
+        Scaffold(modifier = Modifier.systemBarsPadding().fillMaxSize()) { innerPadding ->
+
+            Column(horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier.padding(innerPadding).fillMaxSize()
+                    .background(color = Color.White)){
+
+
+                Text("Test Succesful!")
+
+            }
+
+        }
+
+    }
+
+
+
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GIFTheme {
+        Greeting("Android")
+    }
+}
